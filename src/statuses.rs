0a4aa861db623e73817b74e72881d477cc31862e
Dominic Hamon <dma+github@stripysock.com>
2022-09-30 19:31:08 +0100

    add test for status creation

--- src/statuses.rs
+++ src/statuses.rs
@@ -3,7 +3,7 @@ use std::sync::Mutex;
 
 use actix_web::web::{Json, Path};
 use actix_web::HttpResponse;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -41,9 +41,18 @@ pub struct Status {
 
 impl Status {
     pub fn new(content: &str) -> Self {
+        #[cfg(test)]
+        let id = Uuid::nil();
+        #[cfg(test)]
+        let now = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(42, 0), Utc);
+        #[cfg(not(test))]
+        let id = Uuid::new_v4();
+        #[cfg(not(test))]
+        let now = Utc::now();
+
         Self {
-            id: Uuid::new_v4().to_string(),
-            created_at: Utc::now(),
+            id: id.to_string(),
+            created_at: now,
             content: String::from(content),
             in_reply_to: None,
             sensitive: false,
@@ -130,5 +139,31 @@ pub async fn delete(path: Path<String>) -> HttpResponse {
         None => HttpResponse::NotFound()
             .content_type(APPLICATION_JSON)
             .json(Error::new("Record not found")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{body::to_bytes, http, test, App};
+    use chrono::{TimeZone, Utc};
+
+    #[actix_web::test]
+    async fn test_create_ok() {
+        let app = test::init_service(App::new().service(create)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/api/v1/statuses")
+            .set_json(&CreateStatusRequest {
+                status: String::from("hello world"),
+            })
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), http::StatusCode::CREATED);
+
+        let body_bytes = to_bytes(resp.into_body()).await.unwrap();
+        let status: Status = serde_json::from_slice(&body_bytes).unwrap();
+        assert_eq!(status.id, Uuid::nil().to_string());
+        assert_eq!(status.created_at, Utc.timestamp(42, 0));
     }
 }
