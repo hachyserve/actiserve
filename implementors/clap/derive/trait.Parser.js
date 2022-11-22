7cbc7f72a99dcd16382c8f01a7c748782074c049
dmah42 <510002+dmah42@users.noreply.github.com>
2022-11-22 00:05:01 +0000

    Deploying to gh-pages from @ hachyserve/actiserve@466e74cb35045d84836b209f675ece2b4571d999 🚀

---
+++ implementors/clap/derive/trait.Parser.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["actiserve"] = [{"text":"impl Parser for <a class=\"struct\" href=\"actiserve/struct.Args.html\" title=\"struct actiserve::Args\">Args</a>","synthetic":false,"types":["actiserve::Args"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
